118b152c485e00b0f57134417ddbb776d6a4a9cf
balor001 <balor001@edu.xamk.fi>
2020-10-31 02:30:59 +0200

    Bodyparts now follow new bodyparts
    - When player grows the new instantiated bodyparts follow correctly

---
+++ mato/Assets/Scripts/Player_BodyPart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_BodyPart : MonoBehaviour
+{
+    public int index = 0;
+
+
+
+}
--- mato/Assets/Scripts/Player_GrowScript.cs
+++ mato/Assets/Scripts/Player_GrowScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Player_GrowScript : MonoBehaviour
 {
+    [SerializeField]
     private List<GameObject> matoBodyParts;
     private float zPos;
     private float scaleMultiplier = 1;
@@ -13,7 +14,6 @@ public class Player_GrowScript : MonoBehaviour
     public GameObject bodyPart;
     public GameObject tailPart;
     public GameObject headPart;
-    GameObject previousBodyPart;
 
     public float RotThreshold = 1; //Thresholds to stop moving
     public float DisThreshold = 1.25f;
@@ -24,25 +24,60 @@ public class Player_GrowScript : MonoBehaviour
     Vector3 TargetDirection;
     float Distance; //distance between the object and the target 
 
+    Player_BodyPart bodyPartScript;
+
     // Start is called before the first frame update
     void Start()
     {
         matoBodyParts = new List<GameObject>();
+        bodyPartScript = bodyPart.GetComponent<Player_BodyPart>();
     }
 
     private void Update()
     {
         Debug.Log(matoBodyParts.Count);
-        Follow(bodyPart, headPart);
-        Follow(tailPart, bodyPart);
 
+        // Check that list isn't null
+        if (matoBodyParts != null)
+        {
+            // When there's no bodyparts, tail follows the head
+            if (matoBodyParts.Count == 0)
+            {
+                Follow(tailPart, headPart); // tail follows head
+            }
+            
+            // When Mato is larger than 0
+            else if (matoBodyParts.Count > 0)
+            {
+                // Iterate through every bodypart
+                for (int i = 0; i < matoBodyParts.Count; i++)
+                {
+                    
+                    // first bodypart follows the head
+                    if (i == 0)
+                    {
+                        Follow(matoBodyParts[i], headPart);
+                    }
+
+                    // Other bodyparts follow the next bodypart in the list
+                    else
+                    {
+                        Follow(matoBodyParts[i], matoBodyParts[i - 1]);
+                    }
+                }
+
+                // tail follows the last bodypart in the list
+                Follow(tailPart, matoBodyParts.Last());
+            }
+
+        }
     }
 
-    void Follow(GameObject @object, GameObject target)
+    public void Follow(GameObject @object, GameObject target)
     {
         Distance = Vector3.Distance(@object.transform.position, target.gameObject.transform.position); //distance between target and object
 
-        TargetDirection = Vector3.Scale((target.transform.position - @object.transform.position), new Vector3(1,0,1).normalized);
+        TargetDirection = Vector3.Scale((target.transform.position - @object.transform.position), new Vector3(1, 0, 1).normalized);
 
         // Rotation, Rotate towards target
         Vector3 forward = Vector3.Scale(@object.transform.forward, new Vector3(1, 0, 1)); //forward vector of current object
@@ -58,26 +93,25 @@ public class Player_GrowScript : MonoBehaviour
         {
             @object.transform.position = Vector3.Lerp(@object.transform.position, @object.transform.position + TargetDirection, moveSpeed * Time.deltaTime);
         }
-
     }
-
 
     public void Grow()
     {
-        matoBodyParts.Add(bodyPart);
 
-        zPos = zPos - 1.25f;
+        zPos = zPos - 1.25f; // Sets position
 
         // Maton BodyPartit pitäisi pienentää 0.7 - 0.99
 
-        GameObject bPart = Instantiate(bodyPart);
-        bPart.transform.parent = transform;
-        bPart.transform.localPosition = new Vector3(0, 0, zPos);
-        bPart.transform.rotation = new Quaternion(0, 0, 0, 0);
-        bPart.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1);
-        bPart.name = "BodyPart_" + matoBodyParts.Count;
+        GameObject bPart = Instantiate(bodyPart); // Instantiate new bodypart
+        matoBodyParts.Add(bPart); // Add the new Instantiated object to list
+        bPart.transform.parent = transform; // Instantiate it as a parent of this object
+        bPart.transform.localPosition = transform.position + new Vector3(0, 0, zPos); // Transform it behind the object
+        bPart.transform.rotation = new Quaternion(0, 0, 0, 0); // Zeros the rotation
+        bPart.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1); // Scales down the object
+        bPart.name = "BodyPart_" + matoBodyParts.Count; 
 
         tailPart.transform.parent = transform;
+
         tailPart.transform.localPosition = new Vector3(0, 0, zPos - 1.5f);
     }
 }
