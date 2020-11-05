7d6ebfce6f4332ff34af4d9f19fcbcfb11d7e522
bjola012 <55224564+bjola012@users.noreply.github.com>
2020-11-05 13:56:53 +0200

    EnemyChaseScript

---
+++ mato/Assets/Scripts/Enemy_Logic_Chase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Logic_Chase : MonoBehaviour
+{
+
+    public Transform Player;
+    public int MoveSpeed = 4;
+    public int NoFurtherThan = 10;
+    public int HowClose = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Move towards player
+        //
+        //Look at player
+        transform.LookAt(Player);
+
+        //Move forward
+        if (Vector3.Distance(transform.position, Player.position) >= HowClose)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= NoFurtherThan)
+            {
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }
+    }
+}
+
+
