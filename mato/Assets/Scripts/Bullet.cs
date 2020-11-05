86056f560c28c313c3e048ac6be46a87985f4b38
balor001 <balor001@edu.xamk.fi>
2020-11-05 14:39:45 +0200

    Shooting Mechanics
    - Added basic shooting

---
+++ mato/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 shootDir;
+
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+        Destroy(gameObject, 5f);
+    }
+
+    private void Update()
+    {
+        float moveSpeed = 100f;
+        transform.position += shootDir * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ mato/Assets/Scripts/Player_Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shoot : MonoBehaviour
+{
+    [SerializeField] private Transform bullet;
+    public float bulletForce = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+
+        }
+    }
+
+    void Shoot()
+    {
+        Transform bulletTransform = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        Vector3 shootDirection = transform.forward.normalized;
+        bulletTransform.GetComponent<Bullet>().Setup(shootDirection);
+    }
+}
