319082793771ca2e441ea6f702cf4eff64ef6eaf
balor001 <balor001@edu.xamk.fi>
2020-11-26 14:29:17 +0200

    Updated prefab (again :D)

--- mato/Assets/Scripts/Player_Controller.cs
+++ mato/Assets/Scripts/Player_Controller.cs
@@ -8,19 +8,13 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public bool activeControls = true;
-    
+
     public float moveSpeed = 4f;
     public float rotationSpeed = 45f;
 
     Vector3 forward, right;
 
     public GameObject BodyPartObject;
-
-    /*
-    public GameObject TurretObject;
-    public Transform gunEndPoint;
-    [SerializeField] private Transform bullet;
-    */
 
     public float fireRate = 2f;
     public float nextFire;
@@ -33,13 +27,12 @@ public class Player_Controller : MonoBehaviour
 
     private float horizontalInput;
     private float verticalInput;
-    //private float r_horizontalInput;
-    //private float r_verticalInput;
 
-    public Game_Controller gameController;
+
     public int pickUpCount = 0;
     public int countTreshold = 3;
 
+    public Game_Controller gameController;
     public Joystick joystick;
 
     AudioManager audioManager;
@@ -63,12 +56,8 @@ public class Player_Controller : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal") + joystick.Horizontal;
         verticalInput = Input.GetAxis("Vertical") + joystick.Vertical;
-        
-        //r_horizontalInput = Input.GetAxis("R_Horizontal");
-        //r_verticalInput = Input.GetAxis("R_Vertical");
 
-        // Win condition
-        Debug.Log(gameController.play);
+        // Win condition, player wins when he picks up enough snacks
         if (pickUpCount >= gameController.winCondition && gameController.play)
         {
             gameController.WinLevel();
@@ -79,10 +68,12 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // If controls are active
         if (activeControls)
         {
             transform.position += transform.forward * moveSpeed * Time.fixedDeltaTime; // Go forward at all times.
 
+            // Joystick movement
             if (Mathf.Abs(verticalInput) > 0.25 || Mathf.Abs(horizontalInput) > 0.25)
             {
                 Movement();
@@ -120,53 +111,18 @@ public class Player_Controller : MonoBehaviour
             pickUpCount++;
         }
 
+        // When Colliding with itself
         if (other.gameObject.CompareTag("Player"))
         {
             gameController.GameOver();
             activeControls = false;
         }
 
+        // When colliding with a collidable object(s)
         if (other.gameObject.CompareTag("Collider"))
         {
             gameController.GameOver();
             activeControls = false;
         }
     }
-    
-    #region Old broken Shooting Mechanics
-    /*
-        //if (Mathf.Abs(r_horizontalInput) > 0.25 || Mathf.Abs(r_verticalInput) > 0.25)
-       //{
-       //    Shoot();
-       //}
-     
-    void Shoot()
-    {
-        Vector3 Xshoot = right * moveSpeed * Time.deltaTime * r_horizontalInput;
-        Vector3 YShoot = forward * moveSpeed * Time.deltaTime * r_verticalInput;
-        Vector3 heading = new Vector3((Xshoot.x + YShoot.x), 0, (Xshoot.z + YShoot.z));
-
-
-
-        if (heading == Vector3.zero)
-        {
-            return;
-        }
-        else
-        {
-            Quaternion rotation = Quaternion.LookRotation(heading, Vector3.up);
-            TurretObject.transform.rotation = rotation;
-
-            if (Time.time > nextFire)
-            {
-                nextFire = Time.time + fireRate;
-                Transform bulletTransform = Instantiate(bullet, gunEndPoint.position, Quaternion.identity);
-
-                bulletTransform.GetComponent<Bullet>().Setup(heading);
-            }
-
-        }
-    }
-    */
-    #endregion
 }
